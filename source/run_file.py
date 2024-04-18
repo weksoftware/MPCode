01be60fb8253144e266e62c5d043773b5d0b980d
Denis <denisbasmanov131@gmail.com>
2024-04-18 18:29:41 +0400

    Изменен способ игнорирования пустых строк при запуске файла
    while -> filter

--- source/run_file.py
+++ source/run_file.py
@@ -6,9 +6,9 @@ try:
     with open(input('File: '), 'r', encoding="utf-8") as f:
         file = f.read().splitlines()
 
-    while '' in file:
-        file.remove('')
+    file = list(filter(lambda x: x != '', file))
 
     intrp.run(' '.join(file))
 except:
     input('Run file error')
+    
