21c946092772e45cd170350476a1e45bc1c64c4c
mrwek <68474288+Mister-Wek@users.noreply.github.com>
2024-04-18 12:04:40 +0300

    Обновление скрипта run_file.py

--- source/run_file.py
+++ source/run_file.py
@@ -4,8 +4,11 @@ import intrp
 
 try:
     with open(input('File: '), 'r', encoding="utf-8") as f:
-        file = ' '.join(f.read().splitlines())
-    
-    intrp.run(file)
+        file = f.read().splitlines()
+
+    while '' in file:
+        file.remove('')
+
+    intrp.run(' '.join(file))
 except:
-    input('Error')
+    input('Run file error')
